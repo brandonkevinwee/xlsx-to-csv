43dc7b3314f5289c1e14de8479209a92cd12c929
Brandon Wee <Brandon.Wee@msg-global.com>
2020-02-20 17:00:29 +0800

    Fixed indentation

--- src/main/java/file/Launcher.java
+++ src/main/java/file/Launcher.java
@@ -8,9 +8,9 @@ import file.reader.ReadFiles;
 
 public class Launcher {
 
-	public static void main(String[] args) throws Exception {
-		List<File> files = ReadFiles.getAllExcelFiles();
-		ModifyXlsxFiles modifyFiles = new ModifyXlsxFiles(files);
-		modifyFiles.saveFilesAsCSV(args[0]);
-	}
+  public static void main(String[] args) throws Exception {
+    List<File> files = ReadFiles.getAllExcelFiles();
+    ModifyXlsxFiles modifyFiles = new ModifyXlsxFiles(files);
+    modifyFiles.saveFilesAsCSV(args[0]);
+  }
 }
--- src/main/java/file/edit/ModifyXlsxFiles.java
+++ src/main/java/file/edit/ModifyXlsxFiles.java
@@ -13,38 +13,39 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ModifyXlsxFiles {
-	List<File> files;
+  List<File> files;
 
-	public ModifyXlsxFiles(List<File> files) {
-		this.files = files;
-	}
-	
-	public void saveFilesAsCSV(String separator) throws Exception {
-		for(File file : files) {
-			convertXlsxToCSV(file, separator);
-		}
-	}
-	
-	private void convertXlsxToCSV(File file, String separator) throws Exception {
-		XSSFWorkbook workBook = new XSSFWorkbook(file);
-		XSSFSheet workSheet =  workBook.getSheetAt(0);
-		DataFormatter cellFormatter = new DataFormatter();
-		Iterator<Row> rowIterator = workSheet.rowIterator();
-		Row row;
-		StringBuilder stringData = new StringBuilder();
-		while(rowIterator.hasNext()) {
-			row = rowIterator.next();
-			Iterator<Cell> cellIterator = row.cellIterator();
-			Cell cell;
-			while(cellIterator.hasNext()) {
-				cell = cellIterator.next();
-				stringData.append(cellFormatter.formatCellValue(cell));
-				if(cellIterator.hasNext()) stringData.append(separator);
-			}
-			stringData.append(System.lineSeparator());
-		}
-		File outputFile = new File(FilenameUtils.getBaseName(file.getName()) + ".csv");
-		FileUtils.write(outputFile, stringData, "UTF-8");
-		workBook.close();
-	}
+  public ModifyXlsxFiles(List<File> files) {
+    this.files = files;
+  }
+
+  public void saveFilesAsCSV(String separator) throws Exception {
+    for (File file : files) {
+      convertXlsxToCSV(file, separator);
+    }
+  }
+
+  private void convertXlsxToCSV(File file, String separator) throws Exception {
+    XSSFWorkbook workBook = new XSSFWorkbook(file);
+    XSSFSheet workSheet = workBook.getSheetAt(0);
+    DataFormatter cellFormatter = new DataFormatter();
+    Iterator<Row> rowIterator = workSheet.rowIterator();
+    Row row;
+    StringBuilder stringData = new StringBuilder();
+    while (rowIterator.hasNext()) {
+      row = rowIterator.next();
+      Iterator<Cell> cellIterator = row.cellIterator();
+      Cell cell;
+      while (cellIterator.hasNext()) {
+        cell = cellIterator.next();
+        stringData.append(cellFormatter.formatCellValue(cell));
+        if (cellIterator.hasNext())
+          stringData.append(separator);
+      }
+      stringData.append(System.lineSeparator());
+    }
+    File outputFile = new File(FilenameUtils.getBaseName(file.getName()) + ".csv");
+    FileUtils.write(outputFile, stringData, "UTF-8");
+    workBook.close();
+  }
 }
